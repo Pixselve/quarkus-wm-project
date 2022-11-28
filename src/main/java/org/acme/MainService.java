a7c4baf5f3679a09aa906fc1177bf464c7ea860c
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-28 19:24:53 +0100

    chore: logging

--- src/main/java/org/acme/MainService.java
+++ src/main/java/org/acme/MainService.java
@@ -86,7 +86,7 @@ public class MainService {
   @Incoming("requests")
   public Uni<Void> onRequest(JsonObject quoteRequest) {
     NestedData data = quoteRequest.getJsonObject("data").mapTo(NestedData.class);
-    System.out.printf("✉️ Received request for %s %s with email %s%n", data.firstName, data.lastName, data.email);
+    Log.info("New user registered");
     return reactiveMailer.send(Mail.withHtml(data.email, "❤️ A big welcome from KittenAsso's", welcome.data("firstName", data.firstName).data("lastName", data.lastName).render()));
   }
 }
